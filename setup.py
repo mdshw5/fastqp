156d9a680d7bffc9561e8edb03c16c5a00acedba
Matt Shirley <mdshw5@gmail.com>
2017-05-31 14:50:19 -0400

    Bug fix release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
         name = 'fastqp',
         provides = 'fastqp',
-        version = "0.3",
+        version = "0.3.1",
         author = 'Matthew Shirley',
         author_email = 'mdshw5@gmail.com',
         url = 'http://mattshirley.com',
