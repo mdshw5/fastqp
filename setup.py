a57ace2004acd022d701357be0a8f894c21449f4
Matt Shirley <mdshw5@gmail.com>
2017-09-06 10:58:04 -0400

    Fix whitespace

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 from setuptools import setup
 
-
 setup(
-        name = 'fastqp',
-        provides = 'fastqp',
-        version = "0.3.1",
-        author = 'Matthew Shirley',
-        author_email = 'mdshw5@gmail.com',
-        url = 'http://mattshirley.com',
-        description = 'Simple NGS read quality assessment using Python',
-        license = 'MIT',
-        packages = ['fastqp', 'fastqp.backports'],
-        install_requires = ['six', 'matplotlib', 'numpy', 'simplesam', 'scipy'],
-        entry_points = { 'console_scripts': [ 'fastqp = fastqp.cli:main' ] },
-        classifiers = [
-                "Development Status :: 5 - Production/Stable",
-                "License :: OSI Approved :: MIT License",
-                "Environment :: Console",
-                "Intended Audience :: Science/Research",
-                "Natural Language :: English",
-                "Operating System :: Unix",
-                "Programming Language :: Python :: 2.7",
-                "Programming Language :: Python :: 3.6",
-                "Topic :: Scientific/Engineering :: Bio-Informatics"
-        ]
+    name='fastqp',
+    provides='fastqp',
+    version="0.3.1",
+    author='Matthew Shirley',
+    author_email='mdshw5@gmail.com',
+    url='http://mattshirley.com',
+    description='Simple NGS read quality assessment using Python',
+    license='MIT',
+    packages=['fastqp', 'fastqp.backports'],
+    install_requires=['six', 'matplotlib', 'numpy', 'simplesam', 'scipy'],
+    entry_points={'console_scripts': ['fastqp = fastqp.cli:main']},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ]
 )
