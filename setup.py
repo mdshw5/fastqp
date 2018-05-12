9b40bfe889b5df267c8453f2e0db4b8e929c18e1
Matt Shirley <mdshw5@gmail.com>
2018-05-12 15:09:09 -0400

    Bump package version for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='fastqp',
     provides='fastqp',
-    version="0.3.2",
+    version="0.3.3",
     author='Matthew Shirley',
     author_email='mdshw5@gmail.com',
     url='http://mattshirley.com',
@@ -14,13 +14,9 @@ setup(
     entry_points={'console_scripts': ['fastqp = fastqp.cli:main']},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "Natural Language :: English",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License", "Environment :: Console",
+        "Intended Audience :: Science/Research", "Natural Language :: English",
+        "Operating System :: Unix", "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Topic :: Scientific/Engineering :: Bio-Informatics"
-    ]
-)
+    ])
